f6e47200fceeb640978858f40177accb18c0bd7c
TamNguyen <tamkid2408@gmail.com>
2021-12-11 11:04:20 +0700

    Implement Domain

---
+++ src/KidEng.Domain/Common/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KidEng.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
---
+++ src/KidEng.Domain/Entities/Vocabulary.cs
@@ -0,0 +1,12 @@
+﻿using KidEng.Domain.Common;
+
+namespace KidEng.Domain.Entities
+{
+    public class Vocabulary: EntityBase
+    {
+        public string Word { get; set; }
+        public string Type { get; set; }
+        public string Definition { get; set; }
+        public string Example { get; set; }
+    }
+}
